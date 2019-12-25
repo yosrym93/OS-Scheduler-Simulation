864d2fa3c5b45872b835acf24fd5f7c3d6c24d46
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2019-12-25 13:35:48 +0200

    Updated the statistics file format in utils module.

--- utils/write_stats.py
+++ utils/write_stats.py
@@ -5,11 +5,11 @@ def write_stats(statistics):
     output_file.write("Process Number " +"\t Waiting time" + "\t Turn around"+ "\t Weighted turn around"+'\n')
     sum_weighted = sum_ta = 0
     for i in range(0, processes_count):
-        output_file.write('{0}  \t\t\t{1}  \t\t{2}  \t\t{3}'.format(
+        output_file.write('\t{0}  \t\t{1}  \t\t{2}  \t\t{3}'.format(
             statistics[i].number, statistics[i].waiting_time, statistics[i].turnaround,
             "{0:.2f}".format(statistics[i].weighted_turnaround)))
         output_file.write('\n')
         sum_weighted += statistics[i].weighted_turnaround
         sum_ta += statistics[i].turnaround
-    output_file.write("Average Turn Around" + "\t" + "{0:.2f}".format(sum_ta/processes_count) + '\n')
+    output_file.write('\n'+"Average Turn Around" + "\t" + "{0:.2f}".format(sum_ta/processes_count) + '\n')
     output_file.write("Average Weighted Turn Around" + "\t" + "{0:.2f}".format(sum_weighted/processes_count) + '\n')
